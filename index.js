d8b8c63e9dd938b540b9340ab2c0b61ad3683652
leroygatlin <leroygatlin@gmail.com>
2023-11-17 17:21:46 -0800

    complete array map method

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial =>{
+    let wordsArray = tutorial.split(' ')
+    return wordsArray.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  })
 }
